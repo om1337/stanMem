a2209c5b21d071a36793714cb1c21d54ac7659cc
om <goodies@omnagarkar.com>
2024-07-19 12:27:16 -0500

    Initial Commit

---
+++ stanMem/stanMemApp.swift
@@ -0,0 +1,17 @@
+//
+//  stanMemApp.swift
+//  stanMem
+//
+//  Created by Om Nagarkar on 7/19/24.
+//
+
+import SwiftUI
+
+@main
+struct stanMemApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
